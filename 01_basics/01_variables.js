42f05695c4768cba67fcd05ae912aa5e92765910
Upadhyaya <a.a.upadhyaya@accenture.com>
2024-12-26 10:31:11 +0530

    practised variables and dataTypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,22 @@
+const accountId = 232323
+let emaiId = "archishmaan.upa4@gmail.com"
+var accountPassword = "Password"
+accountCity = "Jaipur"
+let accountState
+
+// accountId = 4 //not allowed in js
+emaiId = "archishmaan@yahoo.com"
+accountPassword = "1234"
+accountCity = "Agra"
+
+console.log(accountId);
+console.log(emaiId);
+console.log(accountPassword);
+console.log(accountCity);
+console.log(accountState);
+
+
+console.table([accountId, emaiId, accountPassword, accountCity, accountState]);
+
+
+
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,23 @@
+"use strict"; // treat all code as newer version of JS
+
+// alert(3+3)  // we are using node.js, not the browser
+// code readability should be high
+// tc39.es ECMAScript standard website for JS
+
+let name = "Archishmaan"; //string dataType
+let age = 23; // number dataType
+let isLoggedIn = true; // boolean dataType
+
+// range of number => 2 to the power of 53
+// bigInt
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined
+// symbol => unique
+
+// object
+
+console.log(typeof "Archishmaan");
+console.log(typeof age);
+console.log(typeof null);
