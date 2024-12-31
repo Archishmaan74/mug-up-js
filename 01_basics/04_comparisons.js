5de117fe815658dd671398c3c65b152240a5f8c4
Upadhyaya <a.a.upadhyaya@accenture.com>
2024-12-31 14:19:16 +0530

    comparisons in js

---
+++ 01_basics/04_comparisons.js
@@ -0,0 +1,15 @@
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 === 1);
+// console.log(2 !== 1);
+
+console.log(null > 0);
+console.log(null == 0);
+console.log(null >= 0); // It's true because null is converted to 0 as comparison operators convert null to a number.
+
+console.log(undefined > 0);
+console.log(undefined == 0);
+console.log(undefined >= 0); // It's false because undefined is converted to NaN as comparison operators convert undefined to a number.
+
+// === is used for strict equality comparison. It doesn't do type conversion.
