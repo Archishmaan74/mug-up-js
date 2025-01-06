b3c3ccf77d313427623505099b3348faef7b4a95
Upadhyaya <a.a.upadhyaya@accenture.com>
2025-01-06 11:06:40 +0530

    fix/01_variables-added additional comments

--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -1,13 +1,13 @@
-const accountId = 232323
-let emaiId = "archishmaan.upa4@gmail.com"
-var accountPassword = "Password"
-accountCity = "Jaipur"
-let accountState
+const accountId = 232323; // Cannot be reassigned or redeclared
+let emaiId = "archishmaan.upa4@gmail.com"; // Can be reassigned but NOT redeclared
+var accountPassword = "Password";
+accountCity = "Jaipur";
+let accountState;
 
 // accountId = 4 //not allowed in js
-emaiId = "archishmaan@yahoo.com"
-accountPassword = "1234"
-accountCity = "Agra"
+emaiId = "archishmaan@yahoo.com";
+accountPassword = "1234";
+accountCity = "Agra";
 
 console.log(accountId);
 console.log(emaiId);
@@ -15,8 +15,4 @@ console.log(accountPassword);
 console.log(accountCity);
 console.log(accountState);
 
-
 console.table([accountId, emaiId, accountPassword, accountCity, accountState]);
-
-
-
