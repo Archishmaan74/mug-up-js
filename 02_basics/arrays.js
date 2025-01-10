fe8c333e5999404dcef49a7279a3b6392e6896cd
Upadhyaya <a.a.upadhyaya@accenture.com>
2025-01-10 13:00:14 +0530

    arrays

---
+++ 02_basics/arrays.js
@@ -0,0 +1,38 @@
+// arrays
+
+const myArr = [1, 2, 3, 4, 5];
+const myHeroes = ["Batman", "Superman", "Wonder Women"];
+
+const myArr2 = new Array(1, 2, 3, 4, 5);
+console.log(myArr[0]); // 1
+
+// Array methods
+
+myArr.push(6); // [1, 2, 3, 4, 5, 6]
+console.log(myArr);
+myArr.push(7); // [1, 2, 3, 4, 5, 6, 7]
+console.log(myArr);
+myArr.pop(); // [1, 2, 3, 4, 5, 6]
+
+myArr.unshift(0); // [0, 1, 2, 3, 4, 5, 6]
+console.log(myArr);
+myArr.shift(); // [1, 2, 3, 4, 5, 6]
+console.log(myArr);
+
+console.log(myArr.includes(3)); // true
+console.log(myArr.indexOf(3)); // 2
+
+console.log(myArr.join(" - ")); // 1 - 2 - 3 - 4 - 5 - 6, join converts array to string
+const newArr = myArr.join(); // "1,2,3,4,5,6"
+console.log(newArr);
+console.log(typeof newArr); // string
+
+// slice and splice
+
+const myArr3 = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+const myArr4 = myArr3.slice(2, 5); // [3, 4, 5]
+console.log(myArr4);
+console.log(myArr3); // [1, 2, 3, 4, 5, 6, 7, 8, 9]
+const myArr5 = myArr3.splice(2, 5); // [3, 4, 5, 6, 7]
+console.log(myArr5);
+console.log(myArr3); // [1, 2, 8, 9]
