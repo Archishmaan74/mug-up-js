edf5e7c222bceb8d8f4311dcf0b9fb4378d11aed
Archishmaan Upadhyaya <archishmaan.upa4@gmail.com>
2025-04-26 21:18:14 +0530

    while and do while loop

---
+++ 05_iterations/two.js
@@ -0,0 +1,23 @@
+// while loop
+
+// let i = 0;
+// while (i < 10) {
+//   console.log(`Value of i is: ${i}`);
+//   i++;
+// }
+
+let myArray = [1, 2, 3, 4, 5];
+
+let arr = 0;
+while (arr < myArray.length) {
+  console.log(`Value of arr is: ${myArray[arr]}`);
+  arr++;
+}
+
+// do while loop
+let score = 11;
+
+do {
+  console.log(`Value of score is: ${score}`);
+  score++;
+} while (score < 5);
