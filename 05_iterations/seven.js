b375dcc9771d210d6a647dc3e205c56cc8483c7e
Upadhyaya <a.a.upadhyaya@accenture.com>
2025-04-29 16:52:12 +0530

    HOF chaining

--- 05_iterations/seven.js
+++ 05_iterations/seven.js
@@ -1,3 +1,5 @@
+// Maps are used to iterate over arrays and create a new array with the results of calling a provided function on every element in the calling array.
+// The map() method creates a new array populated with the results of calling a provided function on every element in the calling array. It does not execute the function for empty elements. The map() method returns a new array, and does not change the original array. The map() method is a higher-order function that takes a callback function as an argument and applies it to each element of the array, returning a new array with the results.
 const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 
 const addTen = numbers.map((num) => {
@@ -11,3 +13,9 @@ numbers.forEach((num) => {
   arr.push(num);
 });
 console.log(arr);
+
+const newNumbers = numbers
+  .map((num) => num * 10)
+  .map((num) => num + 10)
+  .filter((num) => num >= 40);
+console.log(newNumbers); // [40, 50, 60, 70, 80, 90, 100]
