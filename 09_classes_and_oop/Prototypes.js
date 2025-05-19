1573ae18fcdbe3fec5204f6b5e07bd3480e56ba7
Archishmaan Upadhyaya <archishmaan.upa4@gmail.com>
2025-05-19 20:32:54 +0530

    inheritance through prototypes

--- 09_classes_and_oop/Prototypes.js
+++ 09_classes_and_oop/Prototypes.js
@@ -26,3 +26,40 @@ Array.prototype.heyArchies = function () {
 // myHeros.archies()
 // myHeros.heyArchies()
 // heroPower.heyArchies()
+
+// inheritance
+
+const User = {
+  name: "chai",
+  email: "chai@google.com",
+};
+
+const Teacher = {
+  makeVideo: true,
+};
+
+const TeachingSupport = {
+  isAvailable: false,
+};
+
+const TASupport = {
+  makeAssignment: "JS assignment",
+  fullTime: true,
+  __proto__: TeachingSupport,
+};
+
+Teacher.__proto__ = User;
+
+// modern syntax
+Object.setPrototypeOf(TeachingSupport, Teacher);
+
+let anotherUsername = "Archishmaan74     ";
+
+String.prototype.trueLength = function () {
+  console.log(`${this}`);
+  console.log(`True length is: ${this.trim().length}`);
+};
+
+anotherUsername.trueLength();
+"archishmaan".trueLength();
+"iceTea".trueLength();
