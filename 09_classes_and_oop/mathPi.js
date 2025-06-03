41ce2829caee9067e69a9e466c7d9d6abf68b474
Archishmaan Upadhyaya <archishmaan.upa4@gmail.com>
2025-06-03 17:06:05 +0530

    practised getOwnPropertyDescriptor() and defineProperty()

--- 09_classes_and_oop/mathPi.js
+++ 09_classes_and_oop/mathPi.js
@@ -1,10 +1,18 @@
-const descripter = Object.getOwnPropertyDescriptor(Math, "PI");
+const myMath = {
+  PI: "3.24",
+};
+const descripter = Object.getOwnPropertyDescriptor(myMath, "PI");
+console.log(descripter);
 
-// console.log(descripter);
-
-// console.log(Math.PI);
-// Math.PI = 5
-// console.log(Math.PI);
+console.log(myMath.PI);
+Object.defineProperty(myMath, "PI", {
+  writable: true,
+  configurable: false,
+  enumerable: true,
+});
+myMath.PI = 5;
+console.log(myMath.PI);
+console.log(descripter);
 
 const chai = {
   name: "ginger chai",
