61b4df15d6e33b2d7039ffeaae9ab38d48c76774
Archishmaan74 <archishmaan.upa4@gmail.com>
2026-05-12 12:45:05 +0530

    fix/updated poylyfills to be user friendly

--- notes/notes.js
+++ notes/notes.js
@@ -560,9 +560,7 @@ Array.prototype.myMap = function (callback) {
   const result = [];
 
   for (let i = 0; i < this.length; i++) {
-    if (this.hasOwnProperty(i)) {
-      result.push(callback(this[i], i, this));
-    }
+    result.push(callback(this[i], i, this));
   }
 
   return result;
@@ -573,7 +571,7 @@ Array.prototype.myFilter = function (callback) {
   const result = [];
 
   for (let i = 0; i < this.length; i++) {
-    if (this.hasOwnProperty(i) && callback(this[i], i, this)) {
+    if (callback(this[i], i, this)) {
       result.push(this[i]);
     }
   }
@@ -584,17 +582,15 @@ Array.prototype.myFilter = function (callback) {
 // .reduce() polyfill
 Array.prototype.myReduce = function (callback, initialValue) {
   let accumulator = initialValue;
-  let hasInitial = initialValue !== undefined;
+  let startIndex = 0;
 
-  for (let i = 0; i < this.length; i++) {
-    if (this.hasOwnProperty(i)) {
-      if (!hasInitial) {
-        accumulator = this[i];
-        hasInitial = true;
-      } else {
-        accumulator = callback(accumulator, this[i], i, this);
-      }
-    }
+  if (accumulator === undefined) {
+    accumulator = this[0];
+    startIndex = 1;
+  }
+
+  for (let i = startIndex; i < this.length; i++) {
+    accumulator = callback(accumulator, this[i], i, this);
   }
   return accumulator;
 };
